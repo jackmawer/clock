13a7307e39a0145ad489c41a89f87ad759919211
Jack Mawer <jack@mawersoft.co.uk>
2019-02-13 21:05:29 +0000

    Initial commit for GitHub

---
+++ js/galaxy.js
@@ -0,0 +1,158 @@
+document.addEventListener("DOMContentLoaded", function () {
+    return;
+    let paused = false
+    let canvas = document.getElementById("galaxy")
+    let ctx = canvas.getContext("2d")
+    let width = canvas.width = window.innerWidth
+    let height = canvas.height = window.innerHeight
+    let stars = []
+
+    // Helpers
+    let randomRange = (min, max) => min + Math.random() * (max - min)
+    let degreesToRads = (degrees) => degrees / 180 * Math.PI
+    let starSizes = () => randomRange(0.2, 0.9)
+
+    // Configuration
+    let starRadius = 3
+    let starSpeed = 0.2
+    let starCount = randomRange(100, 300)
+    let starDirection = 145
+
+    // Star Generator
+    let starColours = () => {
+        let colours = [
+            "188, 236, 201", // green/white   - Type W & O
+            "188, 227, 236", // blue          - Type B
+            "255, 255, 255", // white         - Type A
+            "247, 250, 181", // yellow/white  - Type F
+            "233, 220, 105", // yellow        - Type G (Sun)
+            "232, 198, 62", // orange        - Type K
+            "232, 152, 62", // orange/red    - Type M
+            "232, 70, 62" // red           - Type N & S
+        ]
+
+        return colours[Math.floor(Math.random() * colours.length)]
+    }
+
+    let star = {
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        radius: 0,
+        opacity: undefined,
+        colour: undefined,
+        trail: undefined,
+
+        create: function (x, y, speed, direction) {
+            let star = Object.create(this)
+            star.x = x
+            star.y = y
+            star.vx = Math.cos(direction) * speed
+            star.vy = Math.sin(direction) * speed
+            star.opacity = Math.random()
+            star.colour = starColours()
+            star.trail = randomRange(0, 10)
+            return star
+        },
+
+        getSpeed: function () {
+            return Math.sqrt(this.vx * this.vx + this.vy * this.vy)
+        },
+
+        getDirection: function () {
+            return Math.atan2(this.vy, this.vx)
+        },
+
+        setDirection: function (heading) {
+            let speed = this.getSpeed()
+            this.vx = Math.cos(heading) * speed
+            this.vy = Math.sin(heading) * speed
+        },
+
+        setSpeed: function (speed) {
+            let heading = this.getDirection()
+            this.vx = Math.cos(heading) * speed
+            this.vy = Math.sin(heading) * speed
+        },
+
+        update: function () {
+            this.x += this.vx
+            this.y += this.vy
+        }
+    }
+
+    // Create Star
+    for (let i = 0; i < starCount; i += 1) {
+        let hashi = star.create(randomRange(0, width), randomRange(0, height), 0, 0)
+        hashi.radius = starRadius * starSizes()
+        hashi.setSpeed(starSpeed)
+        hashi.setDirection(degreesToRads(starDirection))
+        stars.push(hashi)
+    }
+
+    // Draw Star
+    let drawStar = (star) => {
+        ctx.fillStyle = `rgba(${star.colour}, ${star.opacity})`
+        ctx.shadowBlur = star.trail
+        ctx.shadowColor = `rgba(${star.colour}, ${star.opacity})`
+        ctx.beginPath()
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false)
+        ctx.fill()
+
+        ctx.shadowBlur = 0
+    }
+
+    // Draw Scene
+    let update = () => {
+        if (!paused) {
+            ctx.clearRect(0, 0, width, height)
+
+
+            let blueCloud = ctx.createRadialGradient(0, 0, width / 4, width / 2, height / 2, 2000)
+            blueCloud.addColorStop(0, "rgba(73,91,154,0.5)")
+            blueCloud.addColorStop(1, "rgba(0,0,0,0)")
+            ctx.fillStyle = blueCloud
+            ctx.fillRect(0, 0, width, height)
+
+            let purpleCloud = ctx.createRadialGradient(width, height - 200, width / 2, width / 2, height / 2, 2000)
+            purpleCloud.addColorStop(0, "rgba(44,37,60,0.5)")
+            purpleCloud.addColorStop(1, "rgba(0,0,0,0)")
+            ctx.fillStyle = purpleCloud
+            ctx.fillRect(0, 0, width, height)
+
+            /*let comet = ctx.createRadialGradient(width, height, 200, 10, 10, 10)
+                comet.addColorStop(0, "#90D3DA")
+                comet.addColorStop(1, "rgba(0,0,0,0)")
+            ctx.fillStyle = comet
+            ctx.fillRect(0, 0, width, height)*/
+
+            ctx.fill()
+
+            for (let i = 0; i < stars.length; i += 1) {
+                let hashi = stars[i]
+
+                hashi.update()
+                drawStar(hashi)
+
+                if (hashi.x > width) hashi.x = 0
+                if (hashi.x < 0) hashi.x = width
+                if (hashi.y > height) hashi.y = 0
+                if (hashi.y < 0) hashi.y = height
+            }
+        }
+
+        window.requestAnimationFrame(update)
+    }
+
+    // Run
+    update()
+
+    window.onfocus = () => {
+        paused = false
+    }
+
+    window.onblur = () => {
+        paused = true
+    }
+}, false)
---
+++ js/index.js
@@ -0,0 +1,113 @@
+// Functions
+function dateObj(d) { // date parser ...
+    var parts = d.split(/:|\s/),
+        date = new Date();
+    date.setHours(+parts.shift());
+    date.setMinutes(+parts.shift());
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date;
+}
+
+function isSlideDue(startTime, endTime) {
+    var now = new Date();
+
+    //Get date objects
+    var startDate = dateObj(startTime);
+    var endDate = dateObj(endTime);
+
+    var show = now < endDate && now >= startDate ? true : false;
+    return show;
+}
+
+function updateTimeTheme(isDarkened) {
+    if (isDarkened) {
+        /* Night Mode */
+        document.getElementById("date").style.color = "rgba(255, 255, 255, 0.25)";
+        document.getElementById("time").style.color = "rgba(255, 255, 255, 0.3)";
+        document.getElementById("motd").style.color = "rgba(255, 255, 255, 0.25)";
+        document.getElementById("galaxy").opacity = 0.25;
+    } else {
+        /* Day Mode */
+        document.getElementById("date").style.color = "rgba(255, 255, 255, 0.50)";
+        document.getElementById("time").style.color = "rgba(255, 255, 255, 0.75)";
+        document.getElementById("motd").style.color = "rgba(255, 255, 255, 0.50)";
+        document.getElementById("galaxy").opacity = 1;
+    }
+}
+
+function tickTime() {
+    /* Update clock time */
+    document.getElementById("date").innerHTML = new Date().toLocaleString('en-GB', {
+        weekday: 'long',
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+    });
+    document.getElementById("time").innerHTML = new Date().toLocaleString('en-GB', {
+        hour12: true,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+}
+
+function showMotd(motd) {
+    var motdObj = document.getElementById("motd");
+    motdObj.innerHTML = motd;
+}
+
+function showTimeWithMotd(motd, darken) {
+    updateTimeTheme(darken);
+    tickTime();
+    showMotd(motd);
+}
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var weather = "";
+
+function getWeather() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            weather = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://weatherman.mawersoft.co.uk/now?t=" + Math.random(), true);
+    xhttp.send();
+}
+
+// Main code
+var slides = [
+    ["Night", "00:00", "06:00", function () {
+        showTimeWithMotd(weather, true);
+    }],
+    ["Morning", "06:00", "08:00", function () {
+        showTimeWithMotd("Today's gonna be another great day!", true);
+    }],
+    ["Night", "22:00", "24:00", function () {
+        showTimeWithMotd("Goodnight!", true);
+    }],
+    ["Default", "00:00", "23:30", function () {
+        showTimeWithMotd(weather, false);
+    }]
+];
+
+function slideshow() {
+    console.debug("Checking for slides...");
+    for (i = 0; i < slides.length; i++) {
+        if (isSlideDue(slides[i][1], slides[i][2])) {
+            console.debug("Current slide: " + slides[i][0]);
+            slides[i][3]();
+            break; /* 1 slide per time */
+        }
+    }
+}
+
+tickTime();
+getWeather();
+setInterval(slideshow, 1000);
+setInterval(getWeather, 15000);
